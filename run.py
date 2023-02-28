aad4c79c011a468713505748197ca528374ead3c
Johanhby <johanhby@gmail.com>
2023-02-28 12:00:04 +0000

    add ship creation function named create_ships

--- run.py
+++ run.py
@@ -12,4 +12,12 @@ class Board:
             self.row = []
             for x in range(5):
                 self.row.append('O')
-            self.column.append(self.row)       
+            self.column.append(self.row)
+
+    def create_ships(self):
+        self.ships = []
+        while len(self.ships) < 5:
+            ship_row = randint(0, 4)
+            ship_column = randint(0, 4)
+            if (ship_row, ship_column) not in self.ships:
+                self.ships.append((ship_row, ship_column))             
