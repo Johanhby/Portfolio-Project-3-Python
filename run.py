9bc23b770017da5277a30c1bd94780191e27bf68
Johanhby <johanhby@gmail.com>
2023-02-28 11:11:59 +0000

    add create_board function to create the game board

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+class Board:
+    def __init__(self):
+        self.column = []
+        self.row = []
+        self.struck_locations = []
+        self.ships = []
+
+    def create_board(self):
+        for y in range(5):
+            self.row = []
+            for x in range(5):
+                self.row.append('O')
+            self.column.append(self.row)       
